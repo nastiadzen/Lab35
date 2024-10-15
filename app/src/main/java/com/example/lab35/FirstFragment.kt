a9111cfff3dd92642434c1581259acb438c00271
nastiadzen <dzendzelivskan04@gmail.com>
2024-10-15 13:46:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab35/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.example.lab35
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.lab35.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnToSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/lab35/ThirdFragment.kt
@@ -0,0 +1,40 @@
+package com.example.lab35
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.lab35.databinding.FragmentThirdBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment() {
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnToFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_firstFragment)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
